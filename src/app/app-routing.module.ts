c1cdf15d9e328a76d08c48f0f9b0dce8b00d4342
okaminouta <434563z@gmail.com>
2018-03-17 15:14:05 +0200

    4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
+import {CreateComponent} from './create/create.component';
+import {TasksComponent} from './tasks/tasks.component';
 
 
 
 
 const appRoutes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: 'home', component: AppComponent},
+  {path: 'home', component: TasksComponent},
+  {path: 'create', component: CreateComponent},
 ];
 
 @NgModule ({
